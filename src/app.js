7303a2f74afa7536c3adf7c3bcb09b01523ff800
Thiago Leite <thiagoleites@gmail.com>
2024-06-15 00:30:46 -0300

    wip

--- src/app.js
+++ src/app.js
@@ -20,5 +20,5 @@ app.get('/', (req, res) => {
 app.use('/usuarios', usuariosRouter);
 
 app.listen(port, () => {
-    console.log('Servidor rodando na porta ${port}');
+    console.log(`Servidor rodando na porta ${port}`);
 });
