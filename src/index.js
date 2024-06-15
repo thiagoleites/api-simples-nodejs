242ae9c467df9fefbb6927732897f05637cb9fb1
Thiago Leite <thiagoleites@gmail.com>
2024-06-14 23:07:57 -0300

    wip: fix erros in all functions

--- src/index.js
+++ src/index.js
@@ -21,6 +21,12 @@ db.serialize(() => {
     db.run('CREATE TABLE IF NOT EXISTS usuarios ( id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT null, email TEXT NOT NULL)');
 });
 
+// Funcao para validacao de email
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
 app.use(express.json());
 
 // Rota para inserir usuários
@@ -29,6 +35,10 @@ app.post('/usuarios', (req, res) => {
 
     if (!nome || !email) {
         return res.status(400).json({ message: "Os campos são obrigatórios" });
+    }
+
+    if(!isValidEmail(email)) {
+        return res.status(400).json({ message: "O email não é válido!" })
     }
 
     const query = "INSERT INTO usuarios (nome, email) VALUES (?, ?)";
@@ -53,6 +63,30 @@ app.get('/usuarios', (req, res) => {
         }
 
         return res.status(200).json(rows);
+    })
+})
+
+// Rota para deletar usuário
+app.delete("/usuarios/:id", (req, res) => {
+    const { id } = req.params;
+
+    if (!id || isNaN(id)) {
+        return res.status(400).json({ message: "O ID fornecido não é válido!" });
+    }
+
+    const query = "DELETE FROM usuarios WHERE id = ?";
+
+    db.run(query, [id], function(err) {
+        if (err){
+            console.error('Erro ao deletar usuarios', err);
+            return res.status(500).json({ message: "Erro ao deletar usuário"});
+        }
+
+        if(this.changes === 0) {
+            return res.status(404).json({ mesage: "Usuário não encontrado" })
+        }
+
+        res.status(200).json({ message: "Usuario deletado com sucesso"})
     })
 })
 
